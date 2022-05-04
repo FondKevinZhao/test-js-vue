3f82fb3d0e7debf01fbdce5269c4fcde0f78daea
FondKevinZhao <443403633@qq.com>
2022-05-04 17:21:28 +0800

    updated

--- vue系列笔记/刘渊-vue/vue 基础3/App.vue
+++ vue系列笔记/刘渊-vue/vue 基础3/App.vue
@@ -1,10 +1,18 @@
 <template>
-  
+  <div>
+    <Mybutton></Mybutton>
+  </div>
 </template>
 
 <script>
+// APP.vue 是所有组件的父组件
+import Mybutton from './components/MyButton.vue'; // 这里相当于拿到了一个定义组件的配置对象
 export default {
   name: '',
+  components: {
+    // 在这里才使用导入进来的配置对象定义代注册了一个组件
+    Mybutton,
+  }
 }
 </script>
 
