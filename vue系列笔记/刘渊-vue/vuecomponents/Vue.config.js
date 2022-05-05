4cd08e690a6d4f174797bbe5460fab609a123718
FondKevinZhao <443403633@qq.com>
2022-05-05 19:17:39 +0800

    updated

---
+++ vue系列笔记/刘渊-vue/vuecomponents/Vue.config.js
@@ -0,0 +1,15 @@
+// 这个Vue.config.js文件使我们手动添加的，在项目初识化的时候是没有的
+module.exports = {
+  // 关闭eslint的语法检查
+  lintOnSave: false
+  // Vue.config.js中的更多配置，可以在vue.cli网站中查找：https://cli.vuejs.org/zh/config/
+
+  // webpack的五个核心概念：可以看官网
+  /* entry: '',
+  output: {},
+  module: { // loader配置
+    rules: []
+  }, 
+  plugins: [],
+  mode: 'production' or 'development' */
+}
