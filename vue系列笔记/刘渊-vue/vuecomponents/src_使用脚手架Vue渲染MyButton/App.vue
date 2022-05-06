9072da8fb9e97bc0f30cf9f9704497b21a967524
FondKevinZhao <443403633@qq.com>
2022-05-06 20:42:31 +0800

    updated

---
+++ vue系列笔记/刘渊-vue/vuecomponents/src_使用脚手架Vue渲染MyButton/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <MyButton></MyButton>
+  </div>
+</template>
+
+<script>
+// 这个@符，是初始化项目的时候，就自动配置好了的，可以直接使用
+import MyButton from "@/components/MyButton";
+export default {
+  name: "",
+  components: {
+    MyButton,
+  },
+};
+</script>
+
+<style>
+</style>
