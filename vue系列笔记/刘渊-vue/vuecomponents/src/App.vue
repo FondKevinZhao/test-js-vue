b623bc21e58e9607fd7449a92ecd79bbc74a3bf3
FondKevinZhao <443403633@qq.com>
2022-05-07 20:46:09 +0800

    updated

--- vue系列笔记/刘渊-vue/vuecomponents/src/App.vue
+++ vue系列笔记/刘渊-vue/vuecomponents/src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
-    <MyButton></MyButton>
+    <!-- 头部 -->
+    <Header></Header>
+
+    <div class="container">
+      <Add></Add>
+      <List></List>
+    </div>
   </div>
 </template>
 
 <script>
 // 这个@符，是初始化项目的时候，就自动配置好了的，可以直接使用
-import MyButton from "@/components/MyButton";
+import Header from "@/components/Header";
+import Add from "@/components/Add";
+import List from "@/components/List";
+
 export default {
   name: "",
   components: {
-    MyButton,
+    Header,
+    Add,
+    List
   },
 };
 </script>
