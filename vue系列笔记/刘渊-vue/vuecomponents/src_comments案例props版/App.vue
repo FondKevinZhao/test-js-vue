9c93e81616b2c19e32b3b98eea8eb995e8d50227
FondKevinZhao <443403633@qq.com>
2022-05-08 20:01:09 +0800

    updated

---
+++ vue系列笔记/刘渊-vue/vuecomponents/src_comments案例props版/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <!-- 头部 -->
+    <Header></Header>
+
+    <div class="container">
+      <Add :addComment="addComment"></Add>
+      <List :comments="comments" :deleteComment="deleteComment"></List>
+    </div>
+  </div>
+</template>
+
+<script>
+// 这个@符，是初始化项目的时候，就自动配置好了的，可以直接使用
+import Header from "@/components/Header";
+import Add from "@/components/Add";
+import List from "@/components/List";
+
+export default {
+  name: "",
+  components: {
+    Header,
+    Add,
+    List,
+  },
+  data() {
+    return {
+      comments: [
+        { id: 1, content: "vue牛逼", username: "赵丽颖" },
+        { id: 2, content: "vue可以", username: "杨幂" },
+        { id: 3, content: "vue不错", username: "戚薇" },
+      ],
+    };
+  },
+  methods: {
+    // 数据在哪个组件，操作数据的方法就应该在哪个组件
+    // 添加评论
+    addComment(comment) {
+      this.comments.unshift(comment);
+    },
+    // 删除评论
+    /* 
+      删除评论有两种做法：
+        1. 下标法，直接传过来下标，这边根据下标来删除。如：this.comments.splice(index, 1);
+        2. id法：
+          - 根据id找到要删除的是哪个元素
+          - 再用数组的方法删除这个元素
+    */
+    deleteComment(index) {
+      this.comments.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue系列笔记/刘渊-vue/vuecomponents/src_comments案例props版/components/Item.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <li class="list-group-item">
+      <div class="handle">
+        <a href="javascript:;" @click="deleteC">删除</a>
+      </div>
+      <p class="user">
+        <span>{{ comment.username }}</span
+        ><span>说:</span>
+      </p>
+      <p class="centence">{{ comment.content }}</p>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  props: ["comment", "deleteComment", "index"],
+  methods: {
+    // 数据哪，操作数据的方法就在哪。现在数据在App.vue中，所以要在App.vue中去定义删除的方法
+    deleteC() {
+      const confirmDelete = confirm("确定要删除吗？");
+      if (confirmDelete) {
+        // 因为删除的是下标，所以又要从父组件(List.vue)中传一个下标过来
+        this.deleteComment(this.index);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+li {
+  transition: 0.5s;
+  overflow: hidden;
+}
+
+.handle {
+  width: 40px;
+  border: 1px solid #ccc;
+  background: #fff;
+  position: absolute;
+  right: 10px;
+  top: 1px;
+  text-align: center;
+}
+
+.handle a {
+  display: block;
+  text-decoration: none;
+}
+
+.list-group-item .centence {
+  padding: 0px 50px;
+}
+
+.user {
+  font-size: 22px;
+}
+</style>
