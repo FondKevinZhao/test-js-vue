48443aab94bf7794b98b94328c5b2fe55a21a34c
FondKevinZhao <443403633@qq.com>
2022-05-11 20:28:10 +0800

    todos初始化数据完成

--- vue系列笔记/刘渊-vue/vuecomponents/src/components/Item.vue
+++ vue系列笔记/刘渊-vue/vuecomponents/src/components/Item.vue
@@ -1,7 +1,17 @@
 <template>
   <li>
     <label>
-      <input type="checkbox" />
+      <!-- :checked 前面一定要加冒号: -->
+      <!-- checked的第一种写法： -->
+      <input type="checkbox" :checked="todo.isOver"/>
+
+      <!-- checked的第二种写法： 在computed中写 -->
+      <!-- <input type="checkbox" v-model="isChecked"/> -->
+      <!-- 
+        总结：
+          - 多个复选框(checkbox)一组，那么此时每个复选框需要自己填写value的属性值，最终收集数据到一个数组当中
+          - 单个复选框使用，此时不需要填写value值，v-model操作的数据，对应操作的就是复选框的checked属性值
+       -->
       <span>{{ todo.content }}</span>
     </label>
     <button class="btn btn-danger" style="display: none">删除</button>
@@ -11,7 +21,29 @@
 <script>
 export default {
   name: "",
-  props: ["todo"],
+  // props: ["todo"],
+  // props的第二种写法，写对象，可以对传递过来的属性值类型进行限定
+  props: {
+    required: true,
+    todo: Object,
+  },
+  data() {
+    return {
+      // 不要在data中写props传过来的东西，很可能页面展示的时候，数据还没有拿到，data当中不能出现this
+      // isCheck: this.todo.isOver
+    }
+  },
+  computed: {
+    // checked的第二种写法： 在computed中写
+    /* isChecked: {
+      get() {
+        return this.todo.isOver;
+      },
+      set() {
+
+      }
+    } */
+  }
 };
 </script>
 
