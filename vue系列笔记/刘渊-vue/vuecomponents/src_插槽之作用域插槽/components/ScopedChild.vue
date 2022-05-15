7ed0701c723239c9bfbfcb36eaac67fca1f33938
FondKevinZhao <443403633@qq.com>
2022-05-16 01:08:03 +0800

    updated

---
+++ vue系列笔记/刘渊-vue/vuecomponents/src_插槽之作用域插槽/components/ScopedChild.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <!-- slot :todo="todo" 是slot作用域插槽传递，把这个todo的内容传给父组件 -->
+        <slot :todo="todo">
+          {{ todo.content }}
+        </slot>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  props: ['todos']
+}
+</script>
+
+<style>
+
+</style>
