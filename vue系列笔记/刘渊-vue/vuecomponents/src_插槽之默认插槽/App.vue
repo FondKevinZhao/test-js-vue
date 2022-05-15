38357fd0239504ffcf07ec6cef95282de1853f2c
FondKevinZhao <443403633@qq.com>
2022-05-16 00:11:48 +0800

    updated

---
+++ vue系列笔记/刘渊-vue/vuecomponents/src_插槽之默认插槽/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Child>
+      <!-- 通过插槽传东西给儿子，要加template -->
+      <template>
+        <button>点我</button>
+      </template>
+    </Child>
+
+    <Child>
+      <!-- 通过插槽传东西给儿子，要加template -->
+      <template>
+        <p>pp</p>
+      </template>
+    </Child>
+  </div>
+</template>
+
+<script>
+import Child from '@/components/Child.vue'
+export default {
+  name: "",
+  components: {
+    Child
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue系列笔记/刘渊-vue/vuecomponents/src_插槽之默认插槽/components/Child.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>我爱你赵丽颖</h2>
+    <!-- slot是vue当中内置的组件标签 -->
+    <slot>
+      <!-- slot内部的东西是等待着父组件使用的时候给传过来 -->
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
