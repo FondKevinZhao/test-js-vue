dfc80a9fcc42442a41c6a73527344b02f339cd3c
FondKevinZhao <443403633@qq.com>
2022-05-16 23:17:22 +0800

    updated

--- vue系列笔记/刘渊-vue/vuecomponents/src/components/Header.vue
+++ vue系列笔记/刘渊-vue/vuecomponents/src/components/Header.vue
@@ -2,14 +2,37 @@
   <section class="jumbotron">
     <h3 class="jumbotron-heading">Search Github Users</h3>
     <div>
-      <input type="text" placeholder="enter the name you search" />
-      <button>Search</button>
+      <input
+        type="text"
+        placeholder="enter the name you search"
+        v-model="keyword"
+      />
+      <button @click="search">Search</button>
     </div>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "",
+  data() {
+    return {
+      keyword: "",
+    };
+  },
+  methods: {
+    search() {
+      let keyword = this.keyword;
+      if(keyword.trim()) {
+        // 如果keyword是OK的，我们就不要把这个keyword传给Main.vue，在Main.vue中发请求
+        this.$bus.$emit('searchAjax', keyword);
+      } else {
+        alert('关键字必须合法')
+      }
+      keyword = '';
+    }
+  },
+};
 </script>
 
 <style>
--- vue系列笔记/刘渊-vue/vuecomponents/src/components/Main.vue
+++ vue系列笔记/刘渊-vue/vuecomponents/src/components/Main.vue
@@ -1,19 +1,67 @@
 <template>
   <div class="row">
-    <div class="card">
-      <a href="https://github.com/reactjs" target="_blank">
+    <h2 v-if="isFirst">欢迎来搜索</h2>
+    <h2 v-else-if="isLoading">正在搜索中...</h2>
+    <h2 v-else-if="errMsg">请求失败 --- {{ errMsg }}</h2>
+    <div class="card" v-else v-for="user in users" :key="user.url">
+      <a :href="user.userurl" target="_blank">
         <img
-          src="https://avatars.githubusercontent.com/u/6412038?v=3"
+          :src="user.userimg"
           style="width: 100px"
         />
       </a>
-      <p class="card-text">reactjs</p>
+      <p class="card-text">{{ user.username}}</p>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+export default {
+  name: '',
+  data() {
+    return {
+      isFirst: true,
+      isLoading: false,
+      errMsg: '',
+      users: [] // 请求成功后 card 里面的数据
+    }
+  },
+  mounted() {
+    this.$bus.$on('searchAjax', this.getSearchAjax);
+  },
+  methods: {
+    getSearchAjax(keyword) {
+      // 修改页面显示的状态数据，为了让页面显示正在搜索
+      this.isFirst = false;
+      this.isLoading = true;
+      // console.log('keyword',keyword);
+      axios({
+        url: 'https://api.github.com/search/users',
+        method: 'get',
+        // query 参数的写法
+        params: {
+          q: keyword
+        }
+      }).then(response => {
+        this.isLoading = false;
+        // console.log(response.data);
+        this.users = response.data.items.map(item => ({
+          // map函数会返回一个经过处理的新对象
+          // 箭头函数中：对象外面再加一个对象，代表返回的是一个对象
+          username: item.login,
+          userurl: item.html_url,
+          userimg: item.avatar_url
+        }))
+        
+      }).catch(error => {
+        this.isLoading = false;
+        this.errMsg = error.message;
+        console.log(error.message);
+      })
+    }
+  }
+};
 </script>
 
 <style>
