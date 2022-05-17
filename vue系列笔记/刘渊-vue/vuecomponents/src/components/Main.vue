33970bbc8b91c65ac41c0cfb7dde9332ff7f5ee9
FondKevinZhao <443403633@qq.com>
2022-05-17 22:57:55 +0800

    updated

--- vue系列笔记/刘渊-vue/vuecomponents/src/components/Main.vue
+++ vue系列笔记/刘渊-vue/vuecomponents/src/components/Main.vue
@@ -28,33 +28,21 @@ export default {
     this.$bus.$on("searchAjax", this.getSearchAjax);
   },
   methods: {
-    async getSearchAjax(keyword) {
+    async getSearchAjax() {
       // 修改页面显示的状态数据，为了让页面显示正在搜索
       this.isFirst = false;
       this.isLoading = true;
       // console.log('keyword',keyword);
       try {
         const response = await axios({
-          url: "https://api.github.com/search/users",
-          method: "get",
-          // query 参数的写法
-          params: {
-            q: keyword,
-          },
+          // 自己写的服务器地址，会形成跨域，要在vue.config.js的devServer中去解决
+          url: "http://localhost:4000/users/info",
+          method: "get"
         });
-
+        // 能打印代表成功
+        console.log(response.data);
         this.isLoading = false;
-        // console.log(response.data);
-        this.users = response.data.items.map((item) => ({
-          // map函数会返回一个经过处理的新对象
-          // 箭头函数中：对象外面再加一个对象，代表返回的是一个对象
-          username: item.login,
-          userurl: item.html_url,
-          userimg: item.avatar_url,
-        }));
       } catch (error) {
-        this.isLoading = false;
-        this.errMsg = error.message;
         console.log(error.message);
       }
     },
--- vue系列笔记/刘渊-vue/vuecomponents/src/server.js
+++ vue系列笔记/刘渊-vue/vuecomponents/src/server.js
@@ -1,6 +1,20 @@
+// 自己写一个接口，让我的vue来访问(会形成跨域，然后解决跨域)
+
 const express = require('express'); // common.js的引入方式
 const app = express();
+console.log(app.get);
 
-app.get('/', function(req, res) {
-  res.send('Hello World')
+app.get('/users/info', function (req, res) {
+  let response = {
+    status: 200,
+    data: {
+      name: 'zhaoliying',
+      age: 33
+    }
+  }
+  res.send(JSON.stringify(response));
+})
+
+app.listen(4000, function () {
+  console.log('服务器已经启动，在localhost:4000/users/info');
 })
