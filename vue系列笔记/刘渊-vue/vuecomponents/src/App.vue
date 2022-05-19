fa2bce1cd205bce680c7adefdd488d04b9fc2d44
FondKevinZhao <443403633@qq.com>
2022-05-19 22:53:53 +0800

    updated

--- vue系列笔记/刘渊-vue/vuecomponents/src/App.vue
+++ vue系列笔记/刘渊-vue/vuecomponents/src/App.vue
@@ -1,21 +1,15 @@
 <template>
-  <div class="container">
-    <Header></Header>
-    <Main></Main>
+  <div>
+    dd
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header";
-import Main from "@/components/Main";
 export default {
-  name: "",
-  components: {
-    Header,
-    Main,
-  },
-};
+
+}
 </script>
 
 <style>
+
 </style>
--- vue系列笔记/刘渊-vue/vuecomponents/src/main.js
+++ vue系列笔记/刘渊-vue/vuecomponents/src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue';
 import App from '@/App';
-import store from '@/store';
 
 Vue.config.productionTip = false;
 new Vue({
   el: '#app',
-  store,
   render: h => h(App)
 })
