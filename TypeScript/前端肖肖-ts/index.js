68a90cc4810367befc39fcdeb9684e1090f7733a
FondKevinZhao <443403633@qq.com>
2022-06-05 11:35:43 +0800

    updated

---
+++ TypeScript/前端肖肖-ts/index.js
@@ -0,0 +1,6 @@
+var userName = '肖肖小肖';
+var userId = 1;
+userName = '小仙女';
+userId = 2;
+var isShow = false;
+isShow = true;
---
+++ TypeScript/前端肖肖-ts/index.ts
@@ -0,0 +1,7 @@
+var userName:string = '肖肖小肖';
+var userId:number = 1;
+userName = '小仙女';
+userId = 2;
+
+var isShow:boolean = false;
+isShow = true
