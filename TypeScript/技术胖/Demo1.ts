44af1b36d87b194da53ca53aeca6a343c28f800e
FondKevinZhao <443403633@qq.com>
2022-07-07 00:01:36 +0800

    updated

---
+++ TypeScript/技术胖/Demo1.ts
@@ -0,0 +1,10 @@
+function jspang() {
+  let web: string = 'Hello World';
+  console.log(web);
+}
+jspang();
+/* 
+  ts-node：专门用于ts的执行
+    安装：npm i ts-node -g
+    使用：ts-node Demo1.ts
+*/
---
+++ TypeScript/技术胖/Demo3.ts
@@ -0,0 +1,20 @@
+/* 
+  基础静态类型和对象静态类型
+    - 基础静态类型：number, string, null, undefined, boolean, void, symbol 这些都是基础静态类型
+    - 对象静态类型：
+*/
+
+const count: number = 918;
+const myName: string = 'jspang';
+// null, undefined, boolean, void, symbol 这些都是基础静态类型
+
+
+
+// 对象静态类型
+const xiaoJieJie: {
+  name: string,
+  age: number
+} = {
+  name: '大脚',
+  age: 18
+}
