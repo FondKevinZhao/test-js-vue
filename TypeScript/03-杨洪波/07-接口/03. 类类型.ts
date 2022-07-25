0ee4fdd07b77621e13843a717640a31ae6a81297
FondKevinZhao <443403633@qq.com>
2022-07-25 08:43:17 +0800

    updated

--- TypeScript/03-杨洪波/07-接口/03. 类类型.ts
+++ TypeScript/03-杨洪波/07-接口/03. 类类型.ts
@@ -1,6 +1,6 @@
 // 类类型：类也有自己的类型。类的类型可以通过接口来实现
 // 与 C# 或 Java 里接口的基本作用一样，TypeScript 也能用它来明确的强制一个类去符合某种契约。
-/* 
+/*
   类类型: 实现接口
     1. 一个类可以实现多个接口
     2. 一个接口可以继承多个接口
@@ -27,7 +27,7 @@ person.fly(); */
 
 // 再来一个例子：
 // 定义一个接口
-interface IFly {
+/* interface IFly {
   // 该方法没有任何的实现(方法中什么都没有)
   fly(): any, // 后面要加返回类型，否则直接调用回报错
 }
@@ -49,10 +49,47 @@ class Person2 implements IFly, ISwim {
 // 实例化对象
 const person2 = new Person2();
 person2.fly();
-person2.swim();
+person2.swim(); */
 
 /**
  * 总结：
  *  - 类可以通过接口的方式，来定义当前的这个类的类型
  *  - 类可以实现一个接口，类也可以实现多个接口，要注意，接口中的内容都要真正的实现
+ */
+
+
+
+// 接口可以继承其他的多个接口
+// 定义一个接口
+interface IFly {
+  // 该方法没有任何的实现(方法中什么都没有)
+  fly(): any, // 后面要加返回类型，否则直接调用回报错
+}
+// 再定义一个接口
+interface ISwim {
+  swim(): any,
+}
+
+// 定义了一个接口，继承了其他的多个接口
+interface IMyFlyAndSwim extends IFly, ISwim { }
+
+// 定义一个类，直接实现IMyFlyAndSwim 这个接口
+class Person3 implements IMyFlyAndSwim {
+  fly() {
+    console.log('我飞了3');
+  }
+  swim() {
+    console.log('我会游泳啦3');
+  }
+}
+
+const person3 = new Person3();
+person3.fly();
+person3.swim();
+
+
+/**
+ * 总结：
+ *  - 接口和接口之间叫继承(extends)
+ *  - 类和接口之间叫实现(implements)
  */
---
+++ TypeScript/03-杨洪波/08-类/04. 类中的修饰符.ts
@@ -0,0 +1,8 @@
+// 公共、私有与受保护的修饰符
+/**
+ * public: 如果你对其它语言中的类比较了解，就会注意到我们在之前的代码里并没有使用 public 来做修饰；例如，C# 要求必须明确地使用 public 指定成员是可见的。 在 TypeScript 里，成员都默认为 public。
+ * 
+ * private: 当成员被标记成 private 时，它就不能在声明它的类的外部访问。
+ * 
+ * protected: protected 修饰符与 private 修饰符的行为很相似，但有一点不同，protected成员在派生类中仍然可以访问。
+ */
