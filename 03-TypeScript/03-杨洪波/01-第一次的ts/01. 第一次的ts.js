bea61288dd0f0c56945459334b6682ea76787e6e
FondKevinZhao <443403633@qq.com>
2022-08-10 00:14:15 +0800

    updated

--- 03-TypeScript/03-杨洪波/01-第一次的ts/01. 第一次的ts.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    // str这个参数是string类型的
-    function sayHi(str) {
-        return '你好啊' + str;
-    }
-    var text = '小甜甜';
-    console.log(sayHi(text));
-})();
--- 03-TypeScript/03-杨洪波/02-ts在vscode中自动编译/index.ts
+++
@@ -1,7 +0,0 @@
-(()=>{
-  function sayHi(str) {
-    return '啊捏哈赛有' + str;
-  }
-  let text = '小甜甜';
-  console.log(text);
-})()
